febfaed8893b4204a137a7aa9a30d797e65067ce
Ash Tappin <ash.tappin@blackpepper.co.uk>
2019-10-15 10:28:34 +0100

    Add signalR code with a hub unit test

---
+++ SignalRChat.Test/Hubs/ChatHubTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using NUnit.Framework;
+using SignalRChat.Hubs;
+
+namespace SignalRChat.Test.Hubs
+{
+    public class ChatHubTest
+    {
+        [Test]
+        public async Task SendMessage_Broadcasts()
+        {
+            Mock<IHubCallerClients> mockClients = new Mock<IHubCallerClients>();
+            Mock<IClientProxy> mockClientProxy = new Mock<IClientProxy>();
+            ChatHub hub = new ChatHub
+            {
+                Clients = mockClients.Object
+            };
+
+            mockClients.Setup(clients => clients.All).Returns(new Mock<IClientProxy>().Object);
+
+            await hub.SendMessage("User A", "This is a message");
+
+            mockClients.Verify(clients => clients.All, Times.Once);
+            mockClientProxy.Verify(clientProxy => clientProxy.SendCoreAsync("ReceiveMessage",
+                It.Is<object[]>(o => o.Length == 2),
+            default(CancellationToken)));
+        }
+    }
+}
---
+++ SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,27 @@
+﻿const connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function(user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    connection.hub.logging = true;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function(event) {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function(err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
